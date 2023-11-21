81e4a94b628f3f4b4de14f98a1cae13187d8bc77
Wiktor <wiktorkuberax@gmail.com>
2023-11-21 16:01:05 +0100

    advanced queries

--- basic_queries.sql
+++ basic_queries.sql
@@ -1,36 +1,29 @@
--- Na poczatku kazdej sesji - modyfikuje wyswietlana date! gwiazdka vs atrybut obowiazkowy - co jest efektywniejsze - normalnie gwiazdka, chyba ze indeks
--- Indeks zak³adamy zawsze na klucz g³ówny
 ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
 
 
--- Zad 1
 SELECT imie_wroga "WROG", opis_incydentu "PRZEWINA"
 FROM Wrogowie_kocurow
 WHERE EXTRACT(Year FROM data_incydentu) = 2009;
 
 
--- Zad 2
 SELECT imie, funkcja, w_stadku_od "Z NAMI OD"
 FROM Kocury
 WHERE w_stadku_od BETWEEN TO_DATE('2005-09-1') AND TO_DATE('2007-07-31')
 AND plec = 'D';
 
 
--- Zad 3
 SELECT imie_wroga "WROG", gatunek, stopien_wrogosci "STOPIEN WROGOSCI"
 FROM Wrogowie
 WHERE lapowka IS NULL
 ORDER BY stopien_wrogosci;
 
 
--- Zad 4
 SELECT imie || ' zwany ' || pseudo || ' (fun. ' || funkcja || ') lowi myszki w bandzie ' || nr_bandy || ' od ' || w_stadku_od "WSZYSTKO O KOCURACH"
 FROM Kocury
 WHERE plec = 'M'
 ORDER BY w_stadku_od DESC;
 
 
--- Zad 5
 SELECT pseudo, REGEXP_REPLACE(
     REGEXP_REPLACE(pseudo, 'A', '#', 1, 1), 'L', '%', 1, 1     -- string, pattern, replace_str, where_to_start( 1 = start), which_occurrence
 ) "Po wymianie A na # oraz L na %"
@@ -38,19 +31,18 @@ FROM Kocury
 WHERE pseudo LIKE '%A%' AND pseudo LIKE '%L%';
 
 
--- Zad 6
 SELECT imie, w_stadku_od "W stadku",FLOOR(przydzial_myszy*(10/11)) "zjadal",ADD_MONTHS(w_stadku_od, 6) "podwyzka", przydzial_myszy
 FROM Kocury
 WHERE MONTHS_BETWEEN(sysdate, w_stadku_od) / 12 > 14
 AND EXTRACT(Month FROM w_stadku_od) BETWEEN 3 AND 9;
 
--- Zad 7
+
 SELECT imie, NVL(przydzial_myszy, 0)*3 "MYSZY KWARTALNIE", NVL(myszy_extra, 0)*3 "KWARTALNE DODATKI"
 FROM Kocury
 WHERE NVL(przydzial_myszy, 0) > 2*NVL(myszy_extra, 0) AND przydzial_myszy >= 55
 ORDER BY NVL(przydzial_myszy, 0)*3 DESC;
 
--- Zad 8
+
 SELECT imie, DECODE(SIGN(NVL(przydzial_myszy, 0)*12 + NVL(myszy_extra, 0)*12 - 660), -1, 'Ponizej 660',
                                                                                 0, 'Limit',
                                                                                 TO_CHAR(NVL(przydzial_myszy, 0)*12 + NVL(myszy_extra, 0)*12)
@@ -59,8 +51,6 @@ FROM Kocury
 ORDER BY imie;
 
 
--- DEFINE a zmienna
--- Zad 9
 SELECT pseudo, w_stadku_od "W STADKU",
 CASE
     WHEN EXTRACT(Day FROM w_stadku_od) <= 15 AND NEXT_DAY(LAST_DAY(TO_DATE('2023-10-24')) - INTERVAL '7' day, 3) >= TO_DATE('2023-10-24')
@@ -88,6 +78,7 @@ CASE
 END "WYPLATA"
 FROM Kocury
 ORDER BY w_stadku_od;
+
 
 SELECT pseudo, w_stadku_od "W STADKU",
 CASE
@@ -117,7 +108,7 @@ END "WYPLATA"
 FROM Kocury
 ORDER BY w_stadku_od;
 
--- Zad 10
+
 SELECT pseudo || ' - ' ||
 CASE
     WHEN COUNT(pseudo) > 1 THEN 'Nieunikalny'
@@ -136,14 +127,13 @@ FROM Kocury
 GROUP BY szef
 HAVING szef IS NOT NULL;
 
--- Zad 11
+
 SELECT pseudo, COUNT(pseudo) "Liczba wrogow"
 FROM Wrogowie_kocurow
 GROUP BY pseudo
 HAVING COUNT(pseudo) >= 2;
 
 
--- Zad 12
 SELECT 'Liczba kotow = ' " ", COUNT(funkcja) " ", ' lowi jako ' "   ", funkcja "    ", 'i zjada max.' "     ",
 MAX(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) "      ", 'myszy miesiecznie' "          "
 FROM Kocury
@@ -151,20 +141,20 @@ WHERE plec != 'M'
 GROUP BY funkcja
 HAVING funkcja != 'SZEFUNIO' AND AVG(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) > 50;
 
--- Zad 13
+
 SELECT nr_bandy "Nr bandy", plec "Plec", MIN(NVL(przydzial_myszy, 0)) "Minimalny przydzial"
 FROM Kocury
 GROUP BY nr_bandy, plec;
 
 
--- Zad 14
+
 SELECT LEVEL "Poziom", pseudo "Pseudonim", funkcja "Funkcja", nr_bandy "Nr bandy"
 FROM Kocury
 WHERE plec = 'M'
 CONNECT BY PRIOR pseudo = szef
 START WITH funkcja = 'BANDZIOR'
 
--- Zad 15
+
 SELECT LPAD(LEVEL - 1, (LEVEL - 1)* 4 + 1, '===>') || '                ' || imie "Hierarchia",
 CASE
     WHEN szef IS NOT NULL THEN szef
@@ -176,18 +166,11 @@ CONNECT BY PRIOR pseudo = szef
 START WITH funkcja = 'SZEFUNIO';
 
 
--- Zad 16
 SELECT LPAD(' ', (LEVEL - 1)*4, ' ') || pseudo "Droga sluzbowa"
 FROM kocury
 CONNECT BY PRIOR szef = pseudo
 START WITH (MONTHS_BETWEEN(TO_DATE('2023-06-29'), w_stadku_od) / 12) > 14 AND plec = 'M' AND myszy_extra IS NULL;
 
-
-
---SELECT LPAD(' ', (LEVEL - 1)*4, ' ') || pseudo "Droga sluzbowa", CONNECT_BY_ROOT pseudo "root", SYS_CONNECT_BY_PATH(pseudo,'-') "sciezka"
---FROM kocury
---CONNECT BY PRIOR szef = pseudo 
---START WITH (MONTHS_BETWEEN(sysdate, w_stadku_od) / 12) > 14 AND plec = 'M' AND myszy_extra IS NULL;
 
 
 
